fa78d84681cc987a4d222d56923659730b7f04e5
jgan <jgb1017@gmail.com>
2025-10-26 19:10:47 -0500

    Add prologue comments to all files and modify some other comments.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,3 +1,14 @@
+// Prologue
+// Name: App.js
+// Description: Define the application's routes
+// Programmer: Nifemi Lawal
+// Creation date: 10/24/25
+// Revisions: 1.0
+// Pre/post conditions
+//   - Pre: None. 
+//   - Post: The application's routes are defined.
+// Errors: None. 
+
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './components/Header.css';
@@ -5,8 +16,12 @@ import './App.css';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
+// Define our App component
 function App() {
-
+  // Define 3 routes:
+  // 1. / for generic API access
+  // 2. /login for the login page
+  // 3. /dashboard for the dashboard page
   return (
     <Router>
       <Routes> 
@@ -18,4 +33,5 @@ function App() {
   );
 }
 
+// Make the App component available
 export default App;
--- src/client/src/pages/Login.jsx
+++ src/client/src/pages/Login.jsx
@@ -1,3 +1,14 @@
+// Prologue
+// Name: login.jsx
+// Description: Define the login page of our application and its functionality
+// Programmer: Nifemi Lawal
+// Creation date: 10/24/25
+// Revisions: 1.0
+// Pre/post conditions
+//   - Pre: None. 
+//   - Post: None.
+// Errors: All known errors should be handled gracefully. 
+
 // Login page (Login.jsx)
 import React from 'react';
 
@@ -19,4 +30,5 @@ function Login() {
     );
 }
 
+// Make the Login component available for use
 export default Login;
