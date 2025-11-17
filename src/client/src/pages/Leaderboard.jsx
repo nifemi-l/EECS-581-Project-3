55fb906ad23af7ae369f5407d70983679147bbb7
jgan <jgb1017@gmail.com>
2025-11-16 22:39:15 -0600

    Remove about text

--- src/client/src/pages/Leaderboard.jsx
+++ src/client/src/pages/Leaderboard.jsx
@@ -150,21 +150,6 @@ function Leaderboard() {
                             </li>)}
                     </ul>
                 </div>
-
-                {/*About text*/}
-                <div>
-                <p>This is where the leaderboard will be displayed
-                </p>
-                    <h2>Things to do</h2>
-                        <h3>Query the leaderboard / data from the database</h3>
-                            <p>Assuming scores are calculated when the listening history gets updated and stored in the database,
-                                the leaderboard should be able to be queried using a SQL command sorting the scores relation from highest to lowest score
-                            </p>
-                        <h3>Clicking / Viewing Other Profiles</h3>
-                            <p>You should be able to click on other users in the leaderboard to see their listening histories. This might require the username to be at the end of the link / path.
-                            </p>
-                    <h4>Thank you for using Scorify!</h4>
-                </div>
             </div>
         )
     }
